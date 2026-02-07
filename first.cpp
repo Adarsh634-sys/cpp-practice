d63eb74bb354122deee7eb694d0d25974887796f
adarsh <adarshkumar3623@gmail.com>
2026-02-07 23:33:01 +0530

    added first.cpp

---
+++ first.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+using namespace std;
+ int main(){
+   int apple;
+   
+    cout<<" enter the number of apple "<<endl;
+    cin>>apple;
+   
+    cout << "You entered: " << apple << endl;
+
+   
+    return 0;
+ }
+    
+
+ //  taking input two number and addding this number
+
+ # include <iostream>
+ using namespace std;
+  int main(){
+
+   int x;
+   int y;
+   int sum;
+    cout<<"inter the first number"<<endl;
+    cin>>x;
+    cout<<"enter the second number"<<endl;
+    cin>>y; 
+
+  sum= x+y;
+  cout<<" enter the sum of two number: "<<sum<<endl;
+    return 0;
+  }
+  
+
+
+  
